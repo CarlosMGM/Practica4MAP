0669be27afb69fc7d6eaddee4b7d479d1c08b2fb
LluisCS <lluiscan@ucm.es>
2018-03-21 16:59:38 +0100

    Added FightersManager and GunInputComponent

---
+++ ProyectosSDL/HolaSDL/GunInputComponent.cpp
@@ -0,0 +1,31 @@
+#include "GunInputComponent.h"
+
+
+
+GunInputComponent::GunInputComponent(int MaxShots, SDL_Keycode shoot): MaxShots_(MaxShots), shoot_(shoot)
+{
+}
+
+
+GunInputComponent::~GunInputComponent()
+{
+}
+
+void GunInputComponent::handleInput(GameObject * o, Uint32 time, const SDL_Event & event) {
+	
+	Fighter* f = static_cast<Fighter*>(o);
+	if (event.type == SDL_KEYDOWN && event.key.keysym.sym == shoot_) {
+		if (!running) {
+			timer = time + 3000;
+			running = true;
+		}
+		else if (time >= timer) {
+			running = false;
+			currentShots = 0;
+		}
+		if (currentShots < MaxShots_) {
+			send(FighterIsShooting(f, o->getPosition(), o->getDirection() * 4));
+			currentShots++;
+		}
+	}
+}
