b68bce830093145728dd640438a4a4de1bbcd271
CarlosMGM <gonzalem@ucm.es>
2018-03-26 16:30:30 +0200

    AsteroidsManager terminado

--- ProyectosSDL/HolaSDL/AsteroidsManager.cpp
+++ ProyectosSDL/HolaSDL/AsteroidsManager.cpp
@@ -7,6 +7,10 @@ asteroidImage_(ImageRenderer(game->getResources()->getImageTexture(Resources::Im
 
 
 AsteroidsManager::~AsteroidsManager(){
+	while (!asteroids_.empty()) {
+		delete asteroids_.back();
+		asteroids_.pop_back();
+	}
 }
 
 void AsteroidsManager::handleInput(Uint32 time, const SDL_Event & event)
@@ -15,10 +19,16 @@ void AsteroidsManager::handleInput(Uint32 time, const SDL_Event & event)
 
 void AsteroidsManager::update(Uint32 time)
 {
+	for (int i = 0; i < asteroids_.size(); i++)
+		if (asteroids_[i]->isActive())
+			asteroids_[i]->update(time);
 }
 
 void AsteroidsManager::render(Uint32 time)
 {
+	for (int i = 0; i < asteroids_.size(); i++)
+		if (asteroids_[i]->isActive())
+			asteroids_[i]->render(time);
 }
 
 vector<Asteroid*>& AsteroidsManager::getAstroids()
@@ -28,11 +38,54 @@ vector<Asteroid*>& AsteroidsManager::getAstroids()
 
 void AsteroidsManager::receive(Message * msg)
 {
+	switch (msg->id_)
+	{
+	case BULLET_ASTROID_COLLISION:
+		Asteroid * ast = static_cast<BulletAstroidCollision*>(msg)->astroid_;
+		ast->setActive(false);
+		numOfAsteroids_--;
+		if (ast->getGenerations() > 0) {
+			int n = rand() % 3 - 1;
+			for (int i = 0; i < n; i++) {
+				asteroids_.push_back(new Asteroid(game_));
+				asteroids_.back()->setActive(true);
+				asteroids_.back()->addPhysicsComponent(&rotationPhysics_);
+				asteroids_.back()->addPhysicsComponent(&circularPhysics_);
+				asteroids_.back()->addRenderComponent(&asteroidImage_);
+				asteroids_.back()->setGenerations(ast->getGenerations()-1);
+				asteroids_.back()->setPosition(ast->getPosition());
+				double ang = rand();
+				Vector2D v = ast->getVelocity();
+				v.rotate(ang);
+				asteroids_.back()->setVelocity(v);
+			}
+			numOfAsteroids_ += n;
+		}
+		if (numOfAsteroids_ == 0)
+			send(Message(ROUND_OVER));
+		break;
+	case ROUND_START:
+		initAsteroids();
+		break;
+	}
 }
 
 Asteroid * AsteroidsManager::getAsteroid()
 {
-	return nullptr;
+	int i = 0;
+	while (i < asteroids_.size() && asteroids_[i]->isActive())
+		i++;
+	if (i < asteroids_.size())
+		return asteroids_[i];
+	else {
+		asteroids_.push_back(new Asteroid(game_));
+		asteroids_.back()->setActive(false);
+		asteroids_.back()->addPhysicsComponent(&rotationPhysics_);
+		asteroids_.back()->addPhysicsComponent(&circularPhysics_);
+		asteroids_.back()->addRenderComponent(&asteroidImage_);
+		asteroids_.back()->setGenerations(rand() % 2 + 1);
+		return asteroids_.back();
+	}
 }
 
 void AsteroidsManager::initAsteroids()
@@ -42,6 +95,11 @@ void AsteroidsManager::initAsteroids()
 	for (int i = 0; i < 5; i++) {
 		Uint32 x = 0, y = 0;
 		asteroids_.push_back(new Asteroid(game_));
+		asteroids_.back()->setActive(true);
+		asteroids_.back()->addPhysicsComponent(&rotationPhysics_);
+		asteroids_.back()->addPhysicsComponent(&circularPhysics_);
+		asteroids_.back()->addRenderComponent(&asteroidImage_);
+		asteroids_.back()->setGenerations(rand() % 2 + 1);
 		int borde = rand()%4;
 		if (borde == 2)
 			x = game_->getWindowWidth();
